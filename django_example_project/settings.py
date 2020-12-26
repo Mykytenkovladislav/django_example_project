716d402a78cb56175361df8d4ace2b939f27535a
mi4i0 <mykytenkovladislav@gmail.com>
2020-12-26 17:04:43 +0200

    fixed flake8 problems

--- django_example_project/settings.py
+++ django_example_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'cc__u)3ct5l*(dhtbkb3f%=a$ros74g((wv)xey$=hyca2w+hh')
+SECRET_KEY = (os.environ.get("DJANGO_SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
