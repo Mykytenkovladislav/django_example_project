83c72cd2ab2e96097e425a7876d8753e4423d6c4
mi4i0 <mykytenkovladislav@gmail.com>
2021-01-12 20:18:12 +0200

    flake8 fix

--- databases/models.py
+++ databases/models.py
@@ -7,9 +7,17 @@ class Product(models.Model):
     manufacturer = models.CharField(_('manufacturer'), max_length=100)
     date_of_manufacture = models.DateField(_("date of manufacture"), null=True, blank=True)
 
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.product_name}"
+
 
 class City(models.Model):
     name = models.CharField(_('name'), max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.name}"
 
 
 class Client(models.Model):
@@ -19,7 +27,15 @@ class Client(models.Model):
                                      help_text=_("Select a product for this client"))
     city = models.ForeignKey("City", verbose_name=_("city"), on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.last_name}, {self.first_name}"
+
 
 class Provider(models.Model):
     city = models.OneToOneField('City', on_delete=models.CASCADE)
     provider_name = models.TextField(_("provider name"), max_length=10, help_text=_("Provider_name"))
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.provider_name}"
