535c3543f2d881b5d7479bfa17bd1a7835948f18
mi4i0 <mykytenkovladislav@gmail.com>
2021-01-26 20:37:46 +0200

    configured celery to send periodical tasks

--- databases/tasks.py
+++ databases/tasks.py
@@ -1,4 +1,12 @@
-# Create your tasks here
 from celery import shared_task
-
+from datetime import datetime
 from django.core.mail import send_mail as django_send_mail
+
+
+# @shared_task
+# def scraping_task():
+#     django_send_mail('subject', str(datetime.now()), 'noreply@test.com', ['admin@example.com'])
+
+@shared_task
+def scraping_task():
+    django_send_mail('subject', str(datetime.now()), 'noreply@test.com', ['admin@example.com'])
