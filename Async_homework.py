0a58a9faf9ff556101712ee019ac2d2f80ecdd8a
mi4i0 <mykytenkovladislav@gmail.com>
2021-03-28 17:47:17 +0300

    async homework update

--- Async_homework.py
+++ Async_homework.py
@@ -29,7 +29,7 @@ async def weatherstack(api_link, session):
     current_info = response.get('current')
     temperature = current_info.get('temperature', None)
     print(f"Temperature by WEATHERSTACK_API: {temperature}")
-    TEMPERATURE_STORE.append(temperature)
+    return temperature
 
 
 async def metaweather(api_link, session):
@@ -37,23 +37,20 @@ async def metaweather(api_link, session):
     current_info = response.get("consolidated_weather", [{}])[0]
     temperature = current_info.get('the_temp', None)
     print(f"Temperature by METAWEATHER_API: {temperature}")
-    TEMPERATURE_STORE.append(temperature)
+    return temperature
 
 
 async def oceandrivers(api_link, session):
     response = await get_weather_details_async(api_link, session)
     temperature = response.get('TEMPERATURE', None)
     print(f"Temperature by OCEANDRIVERS_API: {temperature}")
-    TEMPERATURE_STORE.append(temperature)
+    return temperature
 
 
 async def running_program():
     async with ClientSession() as session:
-        await asyncio.gather(weatherstack(API[0], session), metaweather(API[1], session), oceandrivers(API[2], session))
-        temperature = 0.0
-        for i in TEMPERATURE_STORE:
-            temperature += i
-        print(f'Median temperature is: {temperature/3}')
+        result = await asyncio.gather(weatherstack(API[0], session), metaweather(API[1], session), oceandrivers(API[2], session))
+        print(f'Median temperature is: {sum(result)/len(result)}')
 
 
 def main():
